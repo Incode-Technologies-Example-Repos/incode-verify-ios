050eb54dcc64d5c8ee3aab0ee710b22188213b05
mendezDLopez <david.mendez@incode.com>
2024-11-14 11:11:46 -0600

    Acme urls and missing label.

--- IncodeVerifyExample/Constants.swift
+++ IncodeVerifyExample/Constants.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 final class Constants {
-  static let clientId = "incodeid_product338"
+  static let clientId = "AcmeSample"
   static let redirectURL = "https%3A%2F%2Fkijak.nl/verificationResult/"
-  static let correlationId = "123"
-  static let environment = "incode-id-5bdz1clk.stage.incodetest.com"
-  static let verificationURL = String(format: "https://%@/?client_id=%@&redirect_url=%@&correlation_id=%@&origin=native",
-                          environment, clientId, redirectURL, correlationId)
+  // static let correlationId = "123"
+  static let environment = "demo.incode.id"
+  static let verificationURL =  String(format: "https://%@/?client_id=%@&redirect_url=%@&origin=native", environment, clientId, redirectURL)
+  // String(format: "https://%@/?client_id=%@&redirect_url=%@&correlation_id=%@&origin=native", environment, clientId, redirectURL, correlationId)
 }
